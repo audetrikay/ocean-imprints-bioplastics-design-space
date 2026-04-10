af58c05b2a39dc0d769c4006d941b48867458658
audetrikay <audedesperies@icloud.com>
2026-04-10 12:56:14 +0200

    Update plot_rigidity.py

--- results/plot_rigidity.py
+++ results/plot_rigidity.py
@@ -1,1 +1,21 @@
+import matplotlib.pyplot as plt
 
+# Data (you can adjust later)
+time = [0, 6, 48, 144]  # hours (0h, 6h, 2d, 6d)
+
+low_glycerine = [12, 9, 5, 3]
+high_glycerine = [18, 14, 10, 7]
+
+plt.figure()
+
+plt.plot(time, low_glycerine, marker='o', label='Low glycerine (2g)')
+plt.plot(time, high_glycerine, marker='o', label='High glycerine (6g)')
+
+plt.xlabel("Immersion time (hours)")
+plt.ylabel("Deflection (mm)")
+plt.title("Evolution of bioplastic rigidity under seawater exposure")
+
+plt.legend()
+
+plt.savefig("rigidity_evolution.png")
+plt.show()
